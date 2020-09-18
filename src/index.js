2d217a130519ded08849bd632277ddae3c34ba07
Roman Lopez Garro <roman927@fedora.localdomain>
2020-09-18 16:27:06 -0300

    fixed some style issues

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const musicController = require('./music/musicController');
+const musicController = require('./musicController');
 
 const PREFIX = "!";
 const watchUrl = "https://www.youtube.com/watch?v=";
 const client = new Discord.Client();
 
+var songs;
+
 client.login(process.env.TOKEN);
 
 client.on("ready", () => {
   console.log("Bot's ready");
+  songs = new Array();
 });
-
-var songs = [];
 
 // Event listener on messages
 client.on('message', async message => {
@@ -23,11 +24,15 @@ client.on('message', async message => {
   const args = message.content.substring(PREFIX.length).split(" ");
 
   if (message.content.startsWith(`${PREFIX}commands`)) {
-    return message.channel.send("Commands are: resume, skip, add, play and stop");
+    return message.channel.send("Commands are: resume, skip, add, play, clear and stop");
   }
 
-  else if (message.content.startsWith(`${PREFIX}resume`)) {
-    if (args.slice(1).length > 0) return message.channel.send('No need to use arguments, just use !resume to resume the queue');
+  else if (message.content.startsWith(`${PREFIX}qresume`)) {
+    let command = message.content;
+    if (!commandIsValid(command)) {
+      return message.channel.send('Wrong command or use of command, to see the commands type !commands');
+    }
+    // Check if posible to resume queue
     else if (songs === undefined || songs.length === 0) {
       message.channel.send("Error: The queue is empty");
       return musicController.stopSong(message);
@@ -37,7 +42,11 @@ client.on('message', async message => {
   }
 
   else if (message.content.startsWith(`${PREFIX}skip`)) {
-    if (args.slice(1).length > 0) return message.channel.send('No need to use arguments, just use !skip to skip the song');
+    let command = message.content;
+    if (!commandIsValid(command)) {
+      return message.channel.send('Wrong command or use of command, to see the commands type !commands');
+    }
+    // Look if there's a song to skip to
     else if (songs.length >= 0 && songs.length <= 1) { 
       return message.channel.send(`Unable to skip, not enough songs. Type !stop if you want to end the stream`);
     }
@@ -46,42 +55,61 @@ client.on('message', async message => {
   }
 
   else if (message.content.startsWith(`${PREFIX}add`)) {
-    if (args.slice(1).length === 0) return message.channel.send('Missing arguments after command');
+
+    let command = message.content;
+    if (!commandIsValid(command)) {
+      return message.channel.send('Wrong command or use of command, to see the commands type !commands');
+    }
+
     else if (args.length === 2 && args[1].startsWith(watchUrl)) {
       songs.push(args[1]);
       return message.channel.send("Succesfuly added new song");
     } else {
+      // Validate vc and continue/stop
       if (!musicController.validateVoiceChannel(message)) return undefined;
+
       let songName = args.slice(1).join('%20');
       let song = await musicController.searchSong(songName);
-      if (song === undefined) return message.channel.send("Reached api's quote limit");
-      songs.push(song.url);
-      return message.channel.send(`Succesfuly added song ${song.title}`);
+      
+      // Check for errors/Add the song
+      if (song === undefined) { 
+        return message.channel.send("Reached api's quote limit");
+      } else {
+        songs.push(song.url);
+        return message.channel.send(`Succesfuly added song ${song.title}`);
+      }
     }
   }
 
   else if (message.content.startsWith(`${PREFIX}play`)) {
-    if (args.slice(1).length === 0) return message.channel.send('Missing arguments after command');
+    
+    let command = message.content;
+    if (!commandIsValid(command))
+        return message.channel.send('Wrong command or use of command, to see the commands type !commands');
 
     if (args.length === 2 && args[1].startsWith(watchUrl)) {
       // TODO: validate url
-      musicController.playSingle(message, args[1]);
+      dispatcher = musicController.playSingle(message, args[1]);
     } else {
       if (!musicController.validateVoiceChannel(message)) return undefined;
+
       let songName = args.slice(1).join('%20');
       let song = await musicController.searchSong(songName);
+
       if (song === undefined || song === false) { 
         return message.channel.send("Reached api's quote limit");
       } else {
         let songUrl = song.url;
-        musicController.playSingle(message, songUrl);
+        dispatcher = musicController.playSingle(message, songUrl);
         message.channel.send(`Now playing: ${song.title}\nSong url:    ${song.url}`);
       }
     }
   }
 
   else if (message.content.startsWith(`${PREFIX}clear`)) {
-    if (args.slice(1).length > 0) return message.channel.send('No need to use arguments, just use !clear to clear the queue');
+    let command = message.content;
+    if (!commandIsValid(command))
+      return message.channel.send('Wrong command or use of command, to see the commands type !commands');
 
     else if (songs === undefined || songs.length < 1) return message.channel.send("Can't clear queue, it's already clear");
     songs = []
@@ -90,10 +118,25 @@ client.on('message', async message => {
   }
 
   else if (message.content.startsWith(`${PREFIX}stop`)) {
+    let command = message.content;
+    if (!commandIsValid(command)) 
+      return message.channel.send('Wrong command or use of command, to see the commands type !commands');
     musicController.stopSong(message, args[1]);
     return;
   }
-  else {
-    message.reply("Wrong command, to see all comands type !commands");
-  }
 });
+
+function commandIsValid(command) {
+  let order = command.slice(1).split(" ");
+  console.log(order);
+  if (order[0] === "play" || order[0] === "add") {
+
+      if (order[1].startsWith(watchUrl) && order.length > 2) return false;
+      else if (order.length === 1) return false;
+      return true;
+  } else {
+
+      if (order.length > 1) return false;
+      return true;
+  }
+}
--- src/musicController.js
+++ src/musicController.js
@@ -1,20 +1,26 @@
 const axios = require('axios');
-const {VoiceChannel} = require('discord.js');
 const ytdl = require('ytdl-core');
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY; 
 
 async function playSingle(message, songUrl) {
+
   const connection = await createConnection(message);
+
   if (connection === undefined) return message.channel.send('You need to be in a voice channel before using commands');
   const dispatcher = connection.play(ytdl(songUrl, {filter: 'audioonly', highWaterMark: 1<<25}))
   .on('finish', () => {
     console.log('Song finished');
   });
+
   dispatcher.setVolume(0.4);
+  console.log(dispatcher);
+  return dispatcher;
 }
 
 async function stopSong(message) {
+
   const voiceChannel = message.member.voice.channel;
+
   if(!validateVoiceChannel(message)) return undefined;
   voiceChannel.leave();
   console.log("leaving channel by order");
@@ -22,7 +28,9 @@ async function stopSong(message) {
 }
 
 async function resumeQueue(message, songs) {
+
   const connection = await createConnection(message);
+
   console.log(`Now playing ${songs[0]}`);
   connection.play(ytdl(songs[0]), {filter: 'audioonly'})
     .on('finish', () => {
@@ -32,7 +40,9 @@ async function resumeQueue(message, songs) {
 }
 
 async function skipQueue(message, songs) {
+
   if (!validateVoiceChannel(message)) return undefined;
+
   songs.shift();
   console.log(`Songs state: ${songs}`);
   resumeQueue(message, songs);
@@ -60,7 +70,6 @@ function validateVoiceChannel(message) {
 }
 
 async function createConnection(message) {
-  // TODO: get to use this function or delete it
   const voiceChannel = message.member.voice.channel;
   if (!validateVoiceChannel(message)) return undefined;
   try {
@@ -69,12 +78,10 @@ async function createConnection(message) {
     console.log(`There was an error while connecting: ${err}`);
     return message.channel.send("There was an error while connecting");
   }
-  console.log(connection);
   return connection;
 }
 
 async function searchSong(keywords) {
-  // TODO: Display message if api quote has been reached
   let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${keywords}&key=${API_KEY}`
   let watch_url = "https://www.youtube.com/watch?v=";
   let song = {"title": '', "url": ''};
@@ -84,27 +91,10 @@ async function searchSong(keywords) {
   } catch (err) {
     return undefined;
   }
-  console.log(resp.data);
   let songId = resp.data.items[0].id.videoId;
   song.title = resp.data.items[0].snippet.title;
   song.url = watch_url.concat(songId);
   return song;
-}
-
-async function muteVolume(message, volumeDirection) {
-  // TODO: - set the volume down by 0.2 points
-  //       - get to receive the dispatcher
-  return;
-}
-
-async function downVolume(message, volumeDirection) {
-  // TODO: set the volume down by 0.2 points
-
-}
-
-async function upVolume(message, volumeDirection) {
-  // TODO: set the volume up by 0.2 points
-
 }
 
 async function addPlaylist(queue, plUrl) {
